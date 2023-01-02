28da94be9d5118ddfe3739c0562d065d9b92a68a
kheireddine mechergui <kheireddine.dev.ops@gmail.com>
2023-01-02 07:53:51 -0800

    feat (controllers) Adding JWT Auth method

--- src/main/java/com/erp/ecommerce/commands/controllers/impls/AccountCommandsController.java
+++ src/main/java/com/erp/ecommerce/commands/controllers/impls/AccountCommandsController.java
@@ -4,13 +4,24 @@ import com.erp.ecommerce.commands.controllers.specs.IAccountCommandsController;
 import com.erp.ecommerce.common.actions.commands.AccountCommands;
 import com.erp.ecommerce.common.dtos.mappers.AccountsMapper;
 import com.erp.ecommerce.common.dtos.requests.CustomerAccountRequestDTO;
+import com.erp.ecommerce.common.dtos.responses.AuthResponseDTO;
 import lombok.extern.log4j.Log4j2;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping({"/accounts"})
@@ -19,6 +30,8 @@ public class AccountCommandsController implements IAccountCommandsController {
     @Autowired
     private CommandGateway commandGateway;
     @Autowired
+    private JwtEncoder jwtEncoder;
+    @Autowired
     private AccountsMapper accountsMapper;
     @Override
     public CompletableFuture<String> addCustomer(CustomerAccountRequestDTO customerAccountRequestDTO) {
@@ -26,5 +39,25 @@ public class AccountCommandsController implements IAccountCommandsController {
         log.info(customerAccountRequestDTO);
         log.info(addCustomerAccountCommand);
         return this.commandGateway.send(addCustomerAccountCommand);
+    }
+
+    @PostMapping({"/auth"})
+    public ResponseEntity<AuthResponseDTO> auth(Authentication authentication) {
+        Instant instant = Instant.now();
+        String scope = authentication.getAuthorities().stream().map(grantedAuthority -> grantedAuthority.getAuthority()).collect(Collectors.joining(","));
+        JwtClaimsSet jwtClaimsSet = JwtClaimsSet.builder()
+                .subject(authentication.getName())
+                .issuedAt(instant)
+                .expiresAt(instant.plus(30, ChronoUnit.DAYS))
+                .issuer("EcommerceService")
+                .claim("scope",scope)
+                .build();
+
+        String jwtAccessToken = this.jwtEncoder.encode(JwtEncoderParameters.from(jwtClaimsSet)).getTokenValue();
+
+        AuthResponseDTO authResponseDTO = new AuthResponseDTO();
+        authResponseDTO.setValid(true);
+        authResponseDTO.setToken(jwtAccessToken);
+        return new ResponseEntity<>(authResponseDTO, HttpStatus.OK);
     }
 }
--- src/main/java/com/erp/ecommerce/commands/controllers/specs/IAccountCommandsController.java
+++ src/main/java/com/erp/ecommerce/commands/controllers/specs/IAccountCommandsController.java
@@ -1,8 +1,10 @@
 package com.erp.ecommerce.commands.controllers.specs;
 
 import com.erp.ecommerce.common.dtos.requests.CustomerAccountRequestDTO;
+import com.erp.ecommerce.common.dtos.responses.AuthResponseDTO;
 import com.erp.ecommerce.common.models.entities.Customer;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -10,6 +12,9 @@ import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
 public interface IAccountCommandsController {
+    @PostMapping({"/auth"})
+    ResponseEntity<AuthResponseDTO> auth(Authentication authentication);
     @PostMapping({"/customers"})
     CompletableFuture<String> addCustomer(@RequestBody CustomerAccountRequestDTO customerAccountRequestDTO);
+
 }

ded56ce2b9eab329055902ff648a33af31907ed8
kheireddine mechergui <kheireddine.dev.ops@gmail.com>
2023-01-08 11:04:57 -0800

    feat (security) Adding JWT utility methods
    - decodeJwt(String token) : Jwt
    - extractAllClaims(String token) : Map<? extends String,Object>
    - extractClaim(String token,String claim) : R
    - extractSubject(String token) : String
    - extractExpiration(String token) : Instant
    - isTokenExpired(String token) : boolean
    - generateAccessToken(String subject, Map<String,Object> claims) : String
    - generateRefreshToken(String subject) : String

--- src/main/java/com/erp/ecommerce/common/configs/security/JwtProperty.java
+++ src/main/java/com/erp/ecommerce/common/configs/security/JwtProperty.java
@@ -6,9 +6,10 @@ import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 
 @ConfigurationProperties(prefix = "jwt")
-public record JwtProperty(AccessToken accessToken,RefreshToken refreshToken,Mapping mapping,Rsa rsa) {
+public record JwtProperty(AccessToken accessToken,RefreshToken refreshToken,Mapping mapping,Rsa rsa,Claims claims) {
     record AccessToken(Long expiration){}
     record RefreshToken(Long expiration){}
+    record Claims(String issuer) {}
     record Mapping(String AuthorityPrefix, String AuthoritiesClaimName, String PrincipalClaimName, String UserClaimName){}
     record Rsa(RSAPublicKey publicKey, RSAPrivateKey privateKey){}
 }

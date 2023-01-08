e9c888b213016550778ca268bbb792ba77cebbfa
kheireddine mechergui <kheireddine.dev.ops@gmail.com>
2023-01-08 07:27:11 -0800

    feat (security) Customizing Spring Security default claim mapping behavior

--- src/main/java/com/erp/ecommerce/common/configs/security/JwtProperty.java
+++ src/main/java/com/erp/ecommerce/common/configs/security/JwtProperty.java
@@ -2,8 +2,13 @@ package com.erp.ecommerce.common.configs.security;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
 @ConfigurationProperties(prefix = "jwt")
-public record JwtProperty(AccessToken accessToken,RefreshToken refreshToken) {
+public record JwtProperty(AccessToken accessToken,RefreshToken refreshToken,Mapping mapping,Rsa rsa) {
     record AccessToken(Long expiration){}
     record RefreshToken(Long expiration){}
+    record Mapping(String AuthorityPrefix, String AuthoritiesClaimName, String PrincipalClaimName, String UserClaimName){}
+    record Rsa(RSAPublicKey publicKey, RSAPrivateKey privateKey){}
 }
--- src/main/java/com/erp/ecommerce/common/configs/security/SecurityConfig.java
+++ src/main/java/com/erp/ecommerce/common/configs/security/SecurityConfig.java
@@ -10,6 +10,7 @@ import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -30,17 +31,25 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 @RequiredArgsConstructor
 public class SecurityConfig {
-
-    private final RsaKeysConfigs keysConfigs;
     private final AccountRepository accountRepository;
+    @Value("${jwt.rsa.public-key}") private RSAPublicKey JWT_RSA_PUBLIC_KEY;
+    @Value("${jwt.rsa.private-key}") private RSAPrivateKey JWT_RSA_PRIVATE_KEY;
+    @Value("${jwt.mapping.authority-prefix}") private String AUTHORITY_PREFIX;
+    @Value("${jwt.mapping.authorities.claim-name}") private String AUTHORITIES_CLAIM_NAME;
+    @Value("${jwt.mapping.principal.claim-name}") private String PRINCIPAL_CLAIM_NAME;
 
     @Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
@@ -80,15 +89,27 @@ public class SecurityConfig {
     }
 
     @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        grantedAuthoritiesConverter.setAuthoritiesClaimName(AUTHORITIES_CLAIM_NAME);
+        grantedAuthoritiesConverter.setAuthorityPrefix(AUTHORITY_PREFIX);
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+        jwtAuthenticationConverter.setPrincipalClaimName(PRINCIPAL_CLAIM_NAME);
+        return jwtAuthenticationConverter;
+    }
+
+    @Bean
     JwtEncoder jwtEncoder() {
-        JWK jwk = new RSAKey.Builder(this.keysConfigs.publicKey()).privateKey(this.keysConfigs.privateKey()).build();
+        JWK jwk = new RSAKey.Builder(JWT_RSA_PUBLIC_KEY).privateKey(JWT_RSA_PRIVATE_KEY).build();
         JWKSource<SecurityContext> jwkSource = new ImmutableJWKSet<>(new JWKSet(jwk));
         return new NimbusJwtEncoder(jwkSource);
     }
 
     @Bean
     JwtDecoder jwtDecoder() {
-        return NimbusJwtDecoder.withPublicKey(this.keysConfigs.publicKey()).build();
+        return NimbusJwtDecoder.withPublicKey(JWT_RSA_PUBLIC_KEY).build();
     }
 
     @Bean

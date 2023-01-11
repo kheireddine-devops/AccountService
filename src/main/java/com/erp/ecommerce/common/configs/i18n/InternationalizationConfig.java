b868a598c46bed66f2ba0f438b0fcf14429a397c
kheireddine mechergui <kheireddine.dev.ops@gmail.com>
2023-01-11 07:11:57 -0800

    feat (i18n) Adding I18n Configs

---
+++ src/main/java/com/erp/ecommerce/common/configs/i18n/InternationalizationConfig.java
@@ -0,0 +1,28 @@
+package com.erp.ecommerce.common.configs.i18n;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import java.util.Locale;
+
+
+@Configuration
+public class InternationalizationConfig {
+
+//    @Bean
+//    public LocaleResolver localeResolver() {
+//        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+//        localeResolver.setDefaultLocale(Locale.US);
+//        return localeResolver;
+//    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
+    }
+}

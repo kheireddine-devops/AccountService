15701a2df5f9d97975fd095e3a3c0753b10645f2
kheireddine mechergui <kheireddine.dev.ops@gmail.com>
2023-01-15 12:45:42 -0800

    feat (swagger) allow swagger endpoints

--- src/main/java/com/erp/ecommerce/common/configs/security/EndPointsConfigs.java
+++ src/main/java/com/erp/ecommerce/common/configs/security/EndPointsConfigs.java
@@ -8,6 +8,17 @@ public class EndPointsConfigs {
             "/accounts/register/{account-type}",
             /* *************** Dev EndPoints *************** */
             "/dev/event-store/{aggregate-id}",
-            "/dev/**"
+            "/dev/**",
+            /* *************** Swagger UI v2 *************** */
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            //* *************** Swagger UI v3 (OpenAPI) *************** */
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
     };
 }

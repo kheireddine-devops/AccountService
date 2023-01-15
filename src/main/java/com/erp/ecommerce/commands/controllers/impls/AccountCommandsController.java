97a8133439a10533a90452d06f6281f9dd93d801
kheireddine mechergui <kheireddine.dev.ops@gmail.com>
2023-01-15 12:35:49 -0800

    feat (controllers) Adding Method : retrieve current user informations

--- src/main/java/com/erp/ecommerce/commands/controllers/impls/AccountCommandsController.java
+++ src/main/java/com/erp/ecommerce/commands/controllers/impls/AccountCommandsController.java
@@ -7,10 +7,20 @@ import com.erp.ecommerce.common.dtos.requests.AccountRequestsDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping({"/accounts"})
@@ -24,5 +34,43 @@ public class AccountCommandsController implements IAccountCommandsController {
     public CompletableFuture<String> addCustomer(AccountRequestsDTO.CreateCustomerAccountDTO createCustomerAccountDTO) {
         AccountCommands.CreateCustomerAccountCommand createCustomerAccountCommand = accountsMapper.fromAddCustomerAccountDTOToCreateCustomerAccountCommand(createCustomerAccountDTO);
         return this.commandGateway.send(createCustomerAccountCommand);
+    }
+
+    @Override
+    public ResponseEntity<Map<String,Object>> getPrincipalInfos(Authentication authentication) {
+        Collection<String> authorities = authentication.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
+        Map<String,Object> infos = Map.of(
+                "username", authentication.getName(),
+                "authorities", authorities,
+                "details",authentication.getDetails()
+//                "principal",authentication.getPrincipal(),
+//                "credentials",authentication.getCredentials()
+        );
+        return new ResponseEntity<>(infos,HttpStatus.OK);
+    }
+
+    public ResponseEntity<Map<String,Object>> getPrincipalInfos(JwtAuthenticationToken principal) {
+        Collection<String> authorities = principal.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
+
+        Map<String,Object> infos = Map.of(
+                "username", principal.getName(),
+                "authorities ",  authorities,
+                "details", principal.getDetails(),
+//                "token", principal.getToken(),
+//                "credentials", principal.getCredentials(),
+                "tokenAttributes", principal.getTokenAttributes()
+        );
+        return new ResponseEntity<>(infos,HttpStatus.OK);
+    }
+
+    public ResponseEntity<Map<String,Object>> getPrincipalInfos(@AuthenticationPrincipal Jwt jwt) {
+        Map<String,Object> infos = Map.of("jwt",jwt);
+        return new ResponseEntity<>(infos, HttpStatus.OK);
     }
 }
--- src/main/java/com/erp/ecommerce/commands/controllers/specs/IAccountCommandsController.java
+++ src/main/java/com/erp/ecommerce/commands/controllers/specs/IAccountCommandsController.java
@@ -2,10 +2,16 @@ package com.erp.ecommerce.commands.controllers.specs;
 
 import com.erp.ecommerce.common.dtos.requests.AccountRequestsDTO;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.validation.Valid;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 @Tag(name = "Account", description = "EndPoints for managing Accounts")
@@ -14,4 +20,12 @@ public interface IAccountCommandsController {
     @PostMapping({"/register/customer"})
     CompletableFuture<String> addCustomer(@RequestBody @Valid AccountRequestsDTO.CreateCustomerAccountDTO createCustomerAccountDTO);
 
+    @GetMapping({"/infos/by-auth"})
+    ResponseEntity<Map<String,Object>> getPrincipalInfos(Authentication authentication);
+
+    @GetMapping({"/infos/by-token"})
+    ResponseEntity<Map<String,Object>> getPrincipalInfos(JwtAuthenticationToken principal);
+
+    @GetMapping({"/infos/by-jwt"})
+    ResponseEntity<Map<String,Object>>  getPrincipalInfos(Jwt jwt);
 }
